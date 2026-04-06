76707d0214690acd61b4a6b23f477e14ee077579
PARK SO YOUNG <itnj.parksy@gmail.com>
2026-04-06 16:04:17 +0900

    ãfirst commit

---
+++ src/main/java/com/portfolio/bleustudio/BleuStudioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.bleustudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleuStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BleuStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/bleustudio/BleuStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.bleustudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BleuStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
