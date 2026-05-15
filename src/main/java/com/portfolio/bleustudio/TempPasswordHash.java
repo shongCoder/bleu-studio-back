e0cc9eaf831cb4acbc56f928779173bf078a44aa
shongCoder <soyeong163@gmail.com>
2026-05-15 15:42:30 +0900

    test

--- src/main/java/com/portfolio/bleustudio/TempPasswordHash.java
+++ src/main/java/com/portfolio/bleustudio/TempPasswordHash.java
@@ -5,6 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class TempPasswordHash {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("bleu0430!2"));
+        System.out.println(encoder.encode("bleu0430!"));
     }
 }
